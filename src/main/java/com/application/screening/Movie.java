1912b458b932f32e0820980b4ee2d59ff0656713
Horace Velmont <dragond7@naver.com>
2020-08-22 21:11:00 +0900

    Add movie

---
+++ src/main/java/com/application/screening/Movie.java
@@ -0,0 +1,17 @@
+package com.application.screening;
+
+import java.time.Duration;
+
+public class Movie {
+    private String title;
+    private Duration runningTime;
+    private Money fee;
+    private DiscountPolicy discountPolicy;
+
+    public Movie(String title, Duration runningTime, Money fee, DiscountPolicy discountPolicy) {
+        this.title = title;
+        this.runningTime = runningTime;
+        this.fee = fee;
+        this.discountPolicy = discountPolicy;
+    }
+}
